14bd1835b756629f3f15de3ae6f9d2465e937164
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-16 06:38:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -6,10 +6,10 @@ class GasStation
   # https://stackoverflow.com/questions/20018784/attr-accessor-vs-attr-reader-instance-variables/20018831
   attr_reader :brand, :unleaded_price
 
-  def initialize(brand, unleaded_price)
-    @brand = brand
-    @unleaded_price = unleaded_price
-  end
+#  def initialize(brand, unleaded_price)
+#    @brand = brand
+#    @unleaded_price = unleaded_price
+#  end
 
 end
 
